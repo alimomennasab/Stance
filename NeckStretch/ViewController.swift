763ba2d0a9487e6f447a4ef88eb15428807bddda
xavierchia <xavier.cpy@gmail.com>
2021-06-01 09:01:59 +0800

    add random completion scene

--- NeckStretch/ViewController.swift
+++ NeckStretch/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet weak var arView: ARView!
     var startAnchor: NeckStretch.Start!
+    var initialBufferAnchor: NeckStretch.InitialBuffer!
     var faceLeftAnchor: NeckStretch.FaceLeft!
     var faceRightAnchor: NeckStretch.FaceRight!
     var lookUpAnchor: NeckStretch.LookUp!
@@ -20,11 +21,15 @@ class ViewController: UIViewController, ARSessionDelegate {
     var tiltLeftAnchor: NeckStretch.TiltLeft!
     var tiltRightAnchor: NeckStretch.TiltRight!
     var chinTuckAnchor: NeckStretch.ChinTuck!
-    var completeAnchor: NeckStretch.Complete1!
+    
+    var complete1Anchor: NeckStretch.Complete1!
+    var complete2Anchor: NeckStretch.Complete2!
+    var complete3Anchor: NeckStretch.Complete3!
+    
     var state = NeckStates.start
-    
     var initialTuck: Float = 0
-    
+    var completeAnchor = ""
+
     @IBOutlet weak var pitchLabel: UILabel!
     @IBOutlet weak var yawLabel: UILabel!
     @IBOutlet weak var rollLabel: UILabel!
@@ -43,8 +48,15 @@ class ViewController: UIViewController, ARSessionDelegate {
                 
     }
     
+    enum CompleteAnchors {
+        static let complete1Anchor = "complete1Anchor"
+        static let complete2Anchor = "complete2Anchor"
+        static let complete3Anchor = "complete3Anchor"
+    }
+    
     enum NeckStates {
         static let start = "start"
+        static let initialBuffer = "initialBuffer"
         static let faceLeft = "faceLeft"
         static let faceRight = "faceRight"
         static let lookUp = "lookUp"
@@ -52,7 +64,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         static let tiltLeft = "tiltLeft"
         static let tiltRight = "tiltRight"
         static let chinTuck = "chinTuck"
-        static let complete = "complete1"
+        static let complete = "complete"
     }
     
     
@@ -77,11 +89,15 @@ class ViewController: UIViewController, ARSessionDelegate {
             if (Double(truncating: leftEyeValue) > 0.5 && Double(truncating: rightEyeValue) < 0.5) ||
                 (Double(truncating: leftEyeValue) < 0.5 && Double(truncating: rightEyeValue) > 0.5) {
                 arView.scene.removeAnchor(startAnchor)
+                
+                // reset tracking is needed in case user was facing camera at a weird angle when opening the app
                 arView.session.run(config, options: [.resetTracking, .removeExistingAnchors])
                 
-                faceLeftAnchor = try! NeckStretch.loadFaceLeft()
-                arView.scene.anchors.append(faceLeftAnchor)
-                self.state = NeckStates.faceLeft
+                // initial buffer is needed as yaw can be an erratic number like -0.3 when tracking is reset, this causes faceLeft scene to be skipped
+                initialBufferAnchor = try! NeckStretch.loadInitialBuffer()
+                initialBufferAnchor.actions.bufferComplete.onAction = bufferToFaceLeft(_:)
+                arView.scene.anchors.append(initialBufferAnchor)
+                state = NeckStates.initialBuffer
             }
         }
         
@@ -154,25 +170,69 @@ class ViewController: UIViewController, ARSessionDelegate {
             let currentTuck = faceAnchor.transform.columns.3[2]
             if -initialTuck + currentTuck < -0.02 {
                 arView.scene.removeAnchor(chinTuckAnchor)
-                completeAnchor = try! NeckStretch.loadComplete1()
-                arView.scene.anchors.append(completeAnchor)
+                addCompleteAnchor()
                 state = NeckStates.complete
             }
         }
         
-//        if state == NeckStates.complete {
-//            let blendShapes = faceAnchor.blendShapes
-//            guard let mouthSmileLeft = blendShapes[.mouthSmileLeft],
-//                  let mouthSmileRight = blendShapes[.mouthSmileRight] else { return }
-//
-//            if (Double(truncating: mouthSmileLeft) > 0.5 && Double(truncating: mouthSmileRight) > 0.5) {
-//                arView.scene.removeAnchor(completeAnchor)
-//                startAnchor = try! NeckStretch.loadStart()
-//                arView.scene.anchors.append(startAnchor)
-//                state = NeckStates.start
-//            }
-//        }
+        if state == NeckStates.complete {
+            let blendShapes = faceAnchor.blendShapes
+            guard let mouthSmileLeft = blendShapes[.mouthSmileLeft],
+                  let mouthSmileRight = blendShapes[.mouthSmileRight] else { return }
+
+            if (Double(truncating: mouthSmileLeft) > 0.5 && Double(truncating: mouthSmileRight) > 0.5) {
+                removeCompleteAnchor()
+                startAnchor = try! NeckStretch.loadStart()
+                arView.scene.anchors.append(startAnchor)
+                state = NeckStates.start
+            }
+        }
         
+    }
+    
+    func bufferToFaceLeft(_ entity: Entity?) {
+        arView.scene.removeAnchor(initialBufferAnchor)
+        faceLeftAnchor = try! NeckStretch.loadFaceLeft()
+        arView.scene.anchors.append(faceLeftAnchor)
+        state = NeckStates.faceLeft
+    }
+    
+    func removeCompleteAnchor() {
+        if completeAnchor == CompleteAnchors.complete1Anchor {
+            arView.scene.removeAnchor(complete1Anchor)
+        }
+        
+        else if completeAnchor == CompleteAnchors.complete2Anchor {
+            arView.scene.removeAnchor(complete2Anchor)
+        }
+        
+        else {
+            arView.scene.removeAnchor(complete3Anchor)
+        }
+    }
+    
+    func addCompleteAnchor() {
+        let randomInt = Int.random(in: 1...3)
+        print(randomInt)
+        
+        if randomInt == 1 {
+            complete1Anchor = try! NeckStretch.loadComplete1()
+            arView.scene.anchors.append(complete1Anchor)
+            completeAnchor = CompleteAnchors.complete1Anchor
+        }
+        
+        else if randomInt == 2 {
+            complete2Anchor = try! NeckStretch.loadComplete2()
+            arView.scene.anchors.append(complete2Anchor)
+            completeAnchor = CompleteAnchors.complete2Anchor
+        }
+        
+        else {
+            complete3Anchor = try! NeckStretch.loadComplete3()
+            arView.scene.anchors.append(complete3Anchor)
+            completeAnchor = CompleteAnchors.complete3Anchor
+        }
+
     }
     
     func updateLabels(faceAnchor: ARFaceAnchor) {
@@ -184,7 +244,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         let yaw = Float(round(1000*(faceAnchor.transform.eulerAnglez.y))/1000)
         
         let roll = -Float(round(1000*(faceAnchor.transform.eulerAnglez.z))/1000)
-        
+                
         tuckLabel.text = "Tuck: \(tuck)"
                 
         pitchLabel.text = "Pitch: \(pitch) "
